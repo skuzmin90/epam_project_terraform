463e480b2ce2454bae7db5e3dc47736ac6a718a7
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-22 16:05:19 +0300

    First commit

--- create_docker_app.sh
+++ create_docker_app.sh
@@ -4,8 +4,8 @@ sudo yum install git docker* -y
 sudo systemctl enable --now docker.service
 sudo mkdir /epam_project && cd /epam_project
 sudo git clone https://github.com/skuzmin90/epam_project_python.git
-sudo cd epam_project_python
-docker build -t webapp .
-docker run --name webapp -p 80:5000 -d \
+cd epam_project_python
+sudo docker build -t webapp .
+sudo docker run --name webapp -p 80:5000 -d \
 -e DB_NAME="${db_name}" -e DB_USER="${db_user}" -e DB_HOST="${db_host}" \
 -e DB_PASSWORD="${db_password}" -e DB_PORT="${db_port}" webapp
