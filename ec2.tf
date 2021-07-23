dd1dd674b2825a88077290929a1d3e9d331b8354
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-23 15:07:59 +0300

    First commit

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,16 @@
-data "template_file" "script-var" {
-  template = "${file("create_docker_app.sh")}"
-  vars = {
-    db_name = "${aws_db_instance.postgresql.name}"
-    db_user = "${aws_db_instance.postgresql.username}"
-    db_password = "${aws_db_instance.postgresql.password}"
-    db_host = "${aws_db_instance.postgresql.address}"
-    db_port = "${aws_db_instance.postgresql.port}"
-  }
-}
-
 resource "aws_instance" "ec2-webapp-1" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   security_groups = [aws_security_group.instance-sg.id]
   subnet_id = aws_subnet.public-subnet-1.id
   associate_public_ip_address =  "true"
-  user_data = "${file("create_docker_app.sh")}"
+  user_data = templatefile("${file("create_docker_app.sh")}", {
+    "db_name" = "${aws_db_instance.postgresql.name}"
+    "db_user" = "${aws_db_instance.postgresql.username}"
+    "db_password" = "${aws_db_instance.postgresql.password}"
+    "db_host" = "${aws_db_instance.postgresql.address}"
+    "db_port" = "${aws_db_instance.postgresql.port}"
+  })
   key_name = "${var.keyname}"
   depends_on = [aws_db_instance.postgresql]
   tags = {
