8faa8811a173c31ee78c4b4575038ac7ea918938
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-23 10:19:58 +0300

    First commit

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2-webapp-1" {
   security_groups = [aws_security_group.instance-sg.id]
   subnet_id = aws_subnet.public-subnet-1.id
   associate_public_ip_address =  "true"
-  user_data = templatefile("create_docker_app.sh", vars) //"${file("create_docker_app.sh")}"
+  user_data = "${file("create_docker_app.sh")}"
   key_name = "${var.keyname}"
   tags = {
     Name = "webapp-1"
