e5fbf58aa2f37f2afd98a6bdd2677b515716909e
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-23 15:51:00 +0300

    First commit

--- create_docker_app.sh
+++ create_docker_app.sh
@@ -8,6 +8,4 @@ cd ./epam_project_python
 sudo pwd >> test.txt
 sudo echo "Print your ${db_name}" >> test.txt
 sudo docker build -t webapp .
-sudo docker run --name webapp -p 80:5000 -d \
--e DB_NAME=${db_name} -e DB_USER=${db_user} -e DB_HOST=${db_host} \
--e DB_PASSWORD=${db_password} -e DB_PORT=${db_port} webapp
+sudo docker run --name webapp -p 80:5000 -d -e DB_NAME=${db_name} -e DB_USER=${db_user} -e DB_HOST=${db_host} -e DB_PASSWORD=${db_password} -e DB_PORT=${db_port} webapp
