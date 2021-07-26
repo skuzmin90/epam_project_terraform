772579c7011ae04f387ac26716843ab14d7ed517
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-26 13:09:50 +0300

    First commit

--- db.tf
+++ db.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "postgresql" {
   skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.database-sg.id]
   db_subnet_group_name = aws_db_subnet_group.db-group.name
+  publicly_accessible = true
   tags = {
     Name = "PostgreSQL"
   }
