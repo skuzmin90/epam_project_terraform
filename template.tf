e6e715306e42aa223e18a567de04724e54c77073
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-26 10:58:18 +0300

    First commit

--- template.tf
+++ template.tf
@@ -1,5 +1,5 @@
 data "template_file" "script-var" {
-  template = "${file("create_docker_app.sh")}"
+  template = "${file("env.dev")}"
   vars = {
     "db_name" = "${aws_db_instance.postgresql.name}"
     "db_user" = "${aws_db_instance.postgresql.username}"
