f1d2d5b27db740ebb1260df1b9514ea0f29c77d2
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-28 15:07:43 +0300

    Add depends_on into template.tf

--- create_docker_app.sh
+++ create_docker_app.sh
@@ -5,6 +5,6 @@ sudo systemctl enable --now docker.service
 sudo mkdir /epam_project && cd /epam_project
 sudo git clone https://github.com/skuzmin90/epam_project_python.git
 cd ./epam_project_python
-sudo sed '/^MAINTAINER*/a ENV DB_HOST=${db_host}' Dockerfile
+sudo echo -e "ENV DB_HOST=${db_host}\nENV DB_PORT=${db_port}\nENV DB_USER=${db_user}\nENV DB_PASSWORD=${db_password}\nENV DB_NAME=${db_name}" >> Dockerfile
 sudo docker build -t webapp .
 sudo docker run -d --name webapp -p 80:5000 webapp
