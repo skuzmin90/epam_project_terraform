86494bdaf951e03d3571486e0fcfaf4fe4c264f3
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-28 16:38:46 +0300

    Add depends_on into template.tf

--- alb.tf
+++ alb.tf
@@ -17,3 +17,16 @@ resource "aws_alb" "alb" {
     Environment = "production"
   }
 }
+resource "aws_alb_target_group" "alb-tg" {
+  vpc_id = aws_vpc.vpc.id
+  protocol = "HTTP"
+  port = 80
+  health_check {
+    protocol = "HTTP"
+    path = "/"
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+  }
+}
