f9b66d64eaadf5a8a5fa4d3fd4166e0427bff532
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-07-30 13:25:58 +0300

    Create target group attachment

--- alb.tf
+++ alb.tf
@@ -20,18 +20,18 @@ resource "aws_alb" "alb" {
 }
 
 resource "aws_alb_target_group" "alb-tg" {
-  name = "alb-target"
-  target_type = "instance"
-  vpc_id = aws_vpc.vpc.id
-  protocol = "HTTP"
-  port = 80
+  name         = "alb-target"
+  target_type  = "instance"
+  vpc_id       = aws_vpc.vpc.id
+  protocol     = "HTTP"
+  port         = 80
   health_check {
-    protocol = "HTTP"
-    path = "/"
-    healthy_threshold = 5
+    protocol            = "HTTP"
+    path                = "/"
+    healthy_threshold   = 5
     unhealthy_threshold = 2
-    timeout = 5
-    interval = 30
+    timeout             = 5
+    interval            = 30
   }
 }
 
@@ -54,7 +54,6 @@ resource "aws_alb_listener" "listener_http" {
   load_balancer_arn = aws_alb.alb.arn
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
     target_group_arn = aws_alb_target_group.alb-tg.arn
     type             = "forward"
