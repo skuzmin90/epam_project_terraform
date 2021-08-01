91695e435ce34471f2873ec6d85d0666d48b9be3
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-01 12:26:38 +0300

    Create IAM role policy and eks-cluster

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -41,7 +41,7 @@ resource "aws_eks_node_group" "nodes-general" {
     max_size = 1
     min_size = 1
   }
-  ami_type = "${var.AMI}"
+  ami_type = "AL2_x86_64"
   capacity_type = "ON_DEMAND"
   disk_size = 20
   force_update_version = false
--- vpc.tf
+++ vpc.tf
@@ -13,9 +13,9 @@ resource "aws_subnet" "public-1" {
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = "true"
   tags = {
-    Name                        = "public-subnet-1"
+    Name                        = "public-us-east-1a"
     "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/alb"    = 1
+    "kubernetes.io/role/elb"    = 1
   }
 }
 resource "aws_subnet" "public-2" {
@@ -24,9 +24,9 @@ resource "aws_subnet" "public-2" {
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "public-subnet-2"
+    Name                        = "public-us-east-1b"
     "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/alb"    = 1
+    "kubernetes.io/role/elb"    = 1
   }
 }
 resource "aws_subnet" "private-1" {
@@ -34,9 +34,9 @@ resource "aws_subnet" "private-1" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "us-east-1a"
   tags = {
-    Name = "private-subnet-1"
-    "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/alb"    = 1
+    Name                              = "private-us-east-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
 
@@ -45,9 +45,9 @@ resource "aws_subnet" "private-2" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "us-east-1b"
   tags = {
-    Name = "private-subnet-2"
-    "kubernetes.io/cluster/eks" = "shared"
-    "kubernetes.io/role/alb"    = 1
+    Name                              = "private-us-east-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
 
