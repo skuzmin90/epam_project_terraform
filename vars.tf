30175bc36e5d39b760b63e1914d97a00d9bdb7a1
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-09 20:12:48 +0300

    Create TF CI

--- vars.tf
+++ vars.tf
@@ -1,17 +1,17 @@
 variable "REGION" {
-  default = "us-east-1"
+  type = string
 }
 
 variable "AMI" {
-  default = "ami-0dc2d3e4c0f9ebd18"
+  type = string
 }
 
 variable "INSTANCE_TYPE" {
-  default = "t2.micro"
+  type = string
 }
 
 variable "KEYNAME" {
-  default = "testKEY"
+  type = string
 }
 
 variable "AWS_ACCESS_KEY_ID" {
@@ -23,7 +23,7 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 
 variable "DB_NAME" {
-  default = "weather"
+  type = string
 }
 
 variable "DB_PASSWORD" {
@@ -31,5 +31,5 @@ variable "DB_PASSWORD" {
 }
 
 variable "DB_USER" {
-  default = "epam"
+  type = string
 }
