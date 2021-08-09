9c11ae4a754af6bb258437ad1fc493697c684160
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-09 09:55:29 +0300

    Create TF CI

--- sg.tf
+++ sg.tf
@@ -1,21 +1,21 @@
 resource "aws_security_group" "instance-sg" {
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -23,21 +23,21 @@ resource "aws_security_group" "instance-sg" {
 resource "aws_security_group" "database-sg" {
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port = 5432
-    protocol = "tcp"
-    to_port = 5432
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -45,21 +45,21 @@ resource "aws_security_group" "database-sg" {
 resource "aws_security_group" "alb-sg" {
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
