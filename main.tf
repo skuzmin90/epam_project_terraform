35b0a20ff54ef47ce6f9f6601ebe9ba5ce11965f
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-10 10:17:36 +0300

    Create TF CI

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "${var.REGION}"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+//  access_key = "${var.AWS_ACCESS_KEY_ID}"
+//  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
