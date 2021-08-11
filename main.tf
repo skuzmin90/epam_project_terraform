a678636e4e6188a014880b2fc9555032bb624221
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-11 09:43:22 +0300

    Integrate GitHub with Terraform cloud

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  backend "remote" {
+    organization = "epam-course"
+
+    workspaces {
+      name = "epam_terraform"
+    }
+  }
+}
+
 provider "aws" {
   region     = "${var.REGION}"
   access_key = "${var.AWS_ACCESS_KEY_ID}"
