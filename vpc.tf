c4f526a96e1db91032da19179d15828d09774801
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-12 09:43:02 +0300

    Kubectl inside Actions

--- vpc.tf
+++ vpc.tf
@@ -10,60 +10,60 @@ resource "aws_vpc" "vpc" {
     Name = "vpc"
   }
 }
-//
-//resource "aws_internet_gateway" "igw" {
-//  vpc_id = aws_vpc.vpc.id
-//  tags = {
-//    Name = "igw"
-//  }
-//}
-//
-//resource "aws_route_table" "public-rt" {
-//  vpc_id = aws_vpc.vpc.id
-//  route {
-//    cidr_block = "0.0.0.0/0"
-//    gateway_id = aws_internet_gateway.igw.id
-//  }
-//  tags = {
-//    Name = "public-rt"
-//  }
-//}
-//
-//resource "aws_route_table" "private-rt-1" {
-//  vpc_id = aws_vpc.vpc.id
-//  route {
-//    cidr_block     = "0.0.0.0/0"
-//    nat_gateway_id = aws_nat_gateway.gw-1.id
-//  }
-//  tags = {
-//    Name = "private-rt-1"
-//  }
-//}
-//
-//resource "aws_route_table" "private-rt-2" {
-//  vpc_id = aws_vpc.vpc.id
-//  route {
-//    cidr_block     = "0.0.0.0/0"
-//    nat_gateway_id = aws_nat_gateway.gw-2.id
-//  }
-//  tags = {
-//    Name = "private-rt-2"
-//  }
-//}
-//
-//resource "aws_route_table_association" "public-association-1" {
-//  route_table_id = aws_route_table.public-rt.id
-//  subnet_id      = aws_subnet.public-1.id
-//}
-//resource "aws_route_table_association" "public-association-2" {
-//  route_table_id = aws_route_table.public-rt.id
-//  subnet_id      = aws_subnet.public-2.id
-//}
-//resource "aws_route_table_association" "private-association-1" {
-//  route_table_id = aws_route_table.private-rt-1.id
-//  subnet_id      = aws_subnet.private-1.id
-//}
-//resource "aws_route_table_association" "private-association-2" {
-//  route_table_id = aws_route_table.private-rt-2.id
-//  subnet_id      = aws_subnet.private-2.id
-//}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private-rt-1" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw-1.id
+  }
+  tags = {
+    Name = "private-rt-1"
+  }
+}
+
+resource "aws_route_table" "private-rt-2" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw-2.id
+  }
+  tags = {
+    Name = "private-rt-2"
+  }
+}
+
+resource "aws_route_table_association" "public-association-1" {
+  route_table_id = aws_route_table.public-rt.id
+  subnet_id      = aws_subnet.public-1.id
+}
+resource "aws_route_table_association" "public-association-2" {
+  route_table_id = aws_route_table.public-rt.id
+  subnet_id      = aws_subnet.public-2.id
+}
+resource "aws_route_table_association" "private-association-1" {
+  route_table_id = aws_route_table.private-rt-1.id
+  subnet_id      = aws_subnet.private-1.id
+}
+resource "aws_route_table_association" "private-association-2" {
+  route_table_id = aws_route_table.private-rt-2.id
+  subnet_id      = aws_subnet.private-2.id
+}
