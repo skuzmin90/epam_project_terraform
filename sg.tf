495a268d7c4b81012efd58f49e23b03fd0fcb64c
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-25 17:52:22 +0300

    Testing deploy

--- sg.tf
+++ sg.tf
@@ -31,6 +31,12 @@ resource "aws_security_group" "database-sg" {
     to_port     = 5432
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     protocol    = "-1"
