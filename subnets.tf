b37ffecbdde8c8d6bf86479326c4286dbd028873
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-09-03 15:20:24 +0300

    Add new env variables and amazon-cloudwatch.yaml

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "public-1" {
   cidr_block              = "10.10.1.0/24"
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name                        = "public-us-east-1a"
+    Name                        = "public-eu-central-1a"
     "kubernetes.io/cluster/eks" = "shared"
     "kubernetes.io/role/elb"    = 1
   }
@@ -13,10 +13,10 @@ resource "aws_subnet" "public-1" {
 resource "aws_subnet" "public-2" {
   cidr_block              = "10.10.2.0/24"
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-1b"
+  availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name                        = "public-us-east-1b"
+    Name                        = "public-eu-central-1b"
     "kubernetes.io/cluster/eks" = "shared"
     "kubernetes.io/role/elb"    = 1
   }
@@ -25,9 +25,9 @@ resource "aws_subnet" "public-2" {
 resource "aws_subnet" "private-1" {
   cidr_block        = "10.10.3.0/24"
   vpc_id            = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
   tags = {
-    Name                              = "private-us-east-1a"
+    Name                              = "private-eu-central-1a"
     "kubernetes.io/cluster/eks"       = "shared"
     "kubernetes.io/role/internal-elb" = 1
   }
@@ -36,9 +36,9 @@ resource "aws_subnet" "private-1" {
 resource "aws_subnet" "private-2" {
   cidr_block        = "10.10.4.0/24"
   vpc_id            = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-central-1b"
   tags = {
-    Name                              = "private-us-east-1b"
+    Name                              = "private-eu-central-1b"
     "kubernetes.io/cluster/eks"       = "shared"
     "kubernetes.io/role/internal-elb" = 1
   }
