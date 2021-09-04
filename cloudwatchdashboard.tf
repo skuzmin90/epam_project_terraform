8fb4943384c38051b31934a396b630cf1281ea4f
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-09-04 12:51:33 +0300

    Add new env variables and amazon-cloudwatch.yaml

---
+++ cloudwatchdashboard.tf
@@ -0,0 +1,54 @@
+resource "aws_cloudwatch_dashboard" "eks-cluster" {
+  dashboard_name = "eks-cluster"
+
+  dashboard_body = <<EOF
+{
+  "widgets": [
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "CPUUtilization",
+            "InstanceId",
+            "i-012345"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "eu-central-1",
+        "title": "EC2 Instance CPU"
+      }
+    },
+    {
+      "type": "text",
+      "x": 0,
+      "y": 7,
+      "width": 3,
+      "height": 3,
+      "properties": {
+        "markdown": "Hello world"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2-cpu" {
+  alarm_name                = "ec2-cpu"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
